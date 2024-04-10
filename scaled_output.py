8007329940d157c04cf629660e7a95c5f055360c
Rohil-Dave <rohil.dave20@imperial.ac.uk>
2024-04-10 17:20:45 +0100

    Scaled rectangle

--- scaled_output.py
+++ scaled_output.py
@@ -1,8 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(0, 10, 100)
-y = np.sin(x)
+# Function to draw a rectangle with the given width and height
+def draw_rectangle(width, height):
+    plt.figure(figsize=(5, 5 * height / width))
+    plt.gca().add_patch(plt.Rectangle((0, 0), width, height, edgecolor='black', facecolor='none'))
+    plt.xlim(0, width)
+    plt.ylim(0, height)
+    plt.gca().set_aspect('equal', adjustable='box')
+    plt.title(f"Rectangle {width}x{height}")
+    plt.axis('off')  # Hide axes
+    plt.show()
 
-plt.plot(x, y)
-plt.show()
+draw_rectangle(200, 100)
