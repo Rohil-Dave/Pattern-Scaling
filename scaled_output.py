95c6f94a0a0cbc637838f4aa2aa25077f2e9e262
Rohil-Dave <rohil.dave20@imperial.ac.uk>
2024-04-11 00:20:08 +0100

    Dashed line to show sleevehead depth

--- scaled_output.py
+++ scaled_output.py
@@ -24,7 +24,7 @@ def draw_tee_pattern(pw, ph, cw, cl, sd, sh, bw, bh, bx, by):
 
 
     # Create a figure and an axis
-    fig, ax = plt.subplots(figsize=(10, 5))
+    fig, ax = plt.subplots(figsize=(20, 10))
 
     # Draw total area of pattern
     main_body = Rectangle((0, 0), width=pw, height=ph, linewidth=1, edgecolor='b', facecolor='none')
@@ -44,11 +44,11 @@ def draw_tee_pattern(pw, ph, cw, cl, sd, sh, bw, bh, bx, by):
 
     # Draw B5 areas, for necklines, and for usage as back neck facing or pockets, etc.
     # Construct B5 straight lines in x direction (width)
-    ax.plot([0, bx], [ph - cl - bh, ph - cl - bh], color='r', lw=1)
-    ax.plot([pw - bx, pw], [ph - cl - bh, ph - cl - bh], color='r', lw=1)
+    ax.plot([0, bx], [ph - cl - bh, ph - cl - bh], color='b', lw=1)
+    ax.plot([pw - bx, pw], [ph - cl - bh, ph - cl - bh], color='b', lw=1)
     # Construct B5 straight lines in y direction (length/height)
-    ax.plot([bw, bw], [ph - cl, ph - cl - by], color='r', lw=1)
-    ax.plot([pw - bw, pw - bw], [ph - cl, ph - cl - by], color='r', lw=1)
+    ax.plot([bw, bw], [ph - cl, ph - cl - by], color='b', lw=1)
+    ax.plot([pw - bw, pw - bw], [ph - cl, ph - cl - by], color='b', lw=1)
 
     B5_left_start = (bx, ph - cl - bh)
     B5_left_control = (bw, ph - cl - bh)   
@@ -90,32 +90,37 @@ def draw_tee_pattern(pw, ph, cw, cl, sd, sh, bw, bh, bx, by):
     sleeveheadpath1 = Path(sleeve1_vertices, sleeveheadcodes)
     sleeveheadpath2 = Path(sleeve2_vertices, sleeveheadcodes)
 
-    sleeveline1 = PathPatch(sleeveheadpath1, fc="none", lw=1, edgecolor='b')
+    sleeveline1 = PathPatch(sleeveheadpath1, fc="none", lw=1, edgecolor='r')
     ax.add_patch(sleeveline1)
-    sleeveline2 = PathPatch(sleeveheadpath2, fc="none", lw=1, edgecolor='b')
+    sleeveline2 = PathPatch(sleeveheadpath2, fc="none", lw=1, edgecolor='r')
     ax.add_patch(sleeveline2)
 
 
 
     # Draw lines connecting sleevehead lines to collar pieces, on pattern from left to right
     # Sample: ax.plot([x1, x2], [y1, y2], color='r')
-    ax.plot([cw, cw + sh], [ph - cl, ph - cl], color='r', lw=1)
-    ax.plot([0.5*pw - cw - sh, 0.5*pw - cw], [ph - cl, ph - cl], color='r', lw=1)
-    ax.plot([0.5*pw + cw, 0.5*pw + cw + sh], [ph - cl, ph - cl], color='r', lw=1)
-    ax.plot([pw - cw, pw - cw - sh], [ph - cl, ph - cl], color='r', lw=1)
+    ax.plot([cw, cw + sh], [ph - cl, ph - cl], color='b', lw=1)
+    ax.plot([0.5*pw - cw - sh, 0.5*pw - cw], [ph - cl, ph - cl], color='b', lw=1)
+    ax.plot([0.5*pw + cw, 0.5*pw + cw + sh], [ph - cl, ph - cl], color='b', lw=1)
+    ax.plot([pw - cw, pw - cw - sh], [ph - cl, ph - cl], color='b', lw=1)
 
+    # Draw dashed lines to show sleevehead area
+    ax.plot([cw + sh, 0.5*pw - cw - sh], [ph - cl, ph - cl], color='b', lw=1, linestyle='dashed')
+    ax.plot([0.5*pw + cw + sh, pw - cw - sh], [ph - cl, ph - cl], color='b', lw=1, linestyle='dashed')
 
 
     # Draw armhole lines
     al = 0.5*(0.5*pw - 2*cw)
-    ax.plot([0.25*pw, 0.25*pw], [ph - cl - sd, ph - cl - sd - al], color='g', lw=1)
-    ax.plot([0.75*pw, 0.75*pw], [ph - cl - sd, ph - cl - sd - al], color='g', lw=1)
+    ax.plot([0.25*pw, 0.25*pw], [ph - cl - sd, ph - cl - sd - al], color='b', lw=1)
+    ax.plot([0.75*pw, 0.75*pw], [ph - cl - sd, ph - cl - sd - al], color='b', lw=1)
 
+
+    # Draw
 
 
     # Setting limits
-    ax.set_xlim(-10, 160)
-    ax.set_ylim(-10, 120)
+    ax.set_xlim(-10, 150)
+    ax.set_ylim(-10, 110)
     ax.set_aspect('equal')
     ax.axis('on')  # Change to off to hide axis
     
