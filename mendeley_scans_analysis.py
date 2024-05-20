acb5663059644f6698d1dbfe29fece74bcd0cfb9
Rohil-Dave <rohil.dave20@imperial.ac.uk>
2024-05-20 22:09:23 +0100

    initial commit

---
+++ mendeley_scans_analysis.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''
+This module is used to add the fabric cut loss metrics and 
+fabric efficiency metrics of the 100 scan Mendeley data file
+'''
+__author__ = 'Rohil J Dave'
+__email__ = 'rohil.dave20@imperial.ac.uk'
