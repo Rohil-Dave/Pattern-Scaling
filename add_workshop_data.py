2c4650806daf55efb1a2fff3338dfa1c84d6d6d9
Rohil-Dave <rohil.dave20@imperial.ac.uk>
2024-05-20 21:28:52 +0100

    Modularised read and analysis of workshop data

--- add_workshop_data.py
+++ add_workshop_data.py
@@ -1,28 +1,84 @@
+#!/usr/bin/env python3
 '''
 This module is used to add the fabric cut loss metrics and 
 fabric efficiency metrics to the ZWS workshop data file
 '''
+__author__ = 'Rohil J Dave'
+__email__ = 'rohil.dave20@imperial.ac.uk'
 
-import os
 import csv
 
 def calculate_ideal_bolt_width(width):
-    '''Calculate the ideal bolt width for a given pattern width on boundaries of 5cm'''
+    '''
+    Calculate the ideal bolt width for a given pattern width on boundaries of 5cm
+    '''
     return width if width % 5 == 0 else width + 5 - width % 5
 
-file_name = './ZWSworkshopData.csv'
+def read_workshop_data():
+    '''
+    Read the data from the workshop, cast all numeric values correctly
+    '''
+    file_name = './ZWSworkshopData.csv'
 
-with open(file_name, mode='r', newline='') as file:
-    reader = csv.DictReader(file)
-    source_data = [row for row in reader]  # Read all data into a list of dictionaries
+    workshop_data = []
+    with open(file_name, mode='r', newline='') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            for key, value in row.items():
+                try:
+                    row[key] = float(value)
+                except ValueError:
+                    row[key] = value
+            workshop_data.append(row)
+    return workshop_data
 
-print('person_id,', 'pattern_width,', 'pattern_height,', 'bolt_width_used,', 'cut_loss_width_used,', 'cut_loss_area_used,', 'cut_loss_width_ideal,', 'cut_loss_area_ideal,', 'efficiency_used,', 'efficiency_ideal')
-for row in source_data:
-    cut_loss_width_used = float(row['bolt_width']) - float(row['pattern_width'])
-    cut_loss_area_used = cut_loss_width_used * float(row['pattern_height'])
-    ideal_bolt_width = calculate_ideal_bolt_width(float(row['pattern_width']))
-    cut_loss_width_ideal = ideal_bolt_width - float(row['pattern_width'])
-    cut_loss_area_ideal = cut_loss_width_ideal * float(row['pattern_height'])
-    efficiency_used = 1 - cut_loss_area_used / (float(row['bolt_width']) * float(row['pattern_height']))
-    efficiency_ideal = 1 - cut_loss_area_ideal / (ideal_bolt_width * float(row['pattern_height']))
-    print(row['person_id'] + ',' + row['pattern_width'] + ',' + row['pattern_height'] + ',' + row['bolt_width'] + ',' + str(cut_loss_width_used) + ',' + str(cut_loss_area_used) + ',' + str(cut_loss_width_ideal) + ',' + str(cut_loss_area_ideal) + ',' + str(efficiency_used) + ',' + str(efficiency_ideal))
+def analyze_data(workshop_data):
+    '''
+    we are going to compute the cut loss width, area, and efficiency for an
+    ideal bolt width (within 5cm) and a bolt that we have (150cm). We will
+    also indicate whether the off cuts can be used as embellishments, etc.
+
+    we can do more here like indicate what kinds of embellishments we can 
+    suggest given the off cut. If we define 'belt' and pocket geometry, we
+    can recoup and increase the efficiency for given bolt width.
+    '''
+
+    analyses = []
+    for row in workshop_data:
+        result = {}
+        result['person_id'] = row['person_id']
+        result['pattern_width'] = row['pattern_width']
+        result['pattern_height'] = row['pattern_height']
+        result['bolt_width_used'] = row['bolt_width']
+        result['cut_loss_width_used'] = row['bolt_width'] - row['pattern_width']
+        result['cut_loss_area_used'] = result['cut_loss_width_used'] * row['pattern_height']
+        result['efficiency_used'] = 1 - result['cut_loss_area_used'] / (row['bolt_width'] *
+            row['pattern_height'])
+        result['ideal_bolt_width'] = calculate_ideal_bolt_width(row['pattern_width'])
+        result['cut_loss_width_ideal'] = result['ideal_bolt_width'] - row['pattern_width']
+        result['cut_loss_area_ideal'] = result['cut_loss_width_ideal'] * row['pattern_height']
+        result['efficiency_ideal'] = 1 - result['cut_loss_area_ideal'] \
+            / (result['ideal_bolt_width'] * row['pattern_height'])
+        analyses.append(result)
+
+    return analyses
+
+def main():
+    '''
+    the main routine to analyze workshop data
+    '''
+
+    workshop_data = read_workshop_data()
+    analyses = analyze_data(workshop_data)
+
+    output_file = 'ZWSworkshopAnalysis.csv'
+    with open(output_file, mode='w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=analyses[0].keys())
+
+        writer.writeheader()
+        for row in analyses:
+            writer.writerow(row)
+
+# Execute main function
+if __name__ == "__main__":
+    main()
