737b139689a5a973cc71c5616cf7e818c3336bb1
Rohil-Dave <rohil.dave20@imperial.ac.uk>
2024-05-22 02:07:55 +0100

    calculate_pattern_height

--- my_scan_analysis.py
+++ my_scan_analysis.py
@@ -66,6 +66,28 @@ def calculate_pattern_width(myscan_data):
     pattern_width = max_bodice_circ + 25 + 6 # add 25cm for ease (fixed for now) and 6cm for hem
     return pattern_width
 
+def calculate_pattern_height(myscan_data):
+    '''
+    Calculate the pattern height from my scan data, deriving shirt length from
+    center back neck height and crotch height
+
+    Use Index 1 to access processed median values from the 5 total scans
+    Example) median_value = myscan_data['measurement name'][1]
+
+    Use Index 0 to access PRE processed median values from the 5 total scans
+    Example) pre_median_value = myscan_data['measurement name'][0]
+    '''
+    cbn_height = myscan_data['N10hb_Neck_Base_Circ_CB_Height_001hb'][1]
+    crotch_height = myscan_data['C10h_Crotch_Height_036h'][1]
+
+    # Step 1: Calculate shirt length
+    raw_shirt_length = cbn_height - crotch_height
+    # Step 2: Round up to the nearest 0.5cm
+    shirt_length = math.ceil(raw_shirt_length * 2) / 2
+    # Step 3: Calculate pattern height with collar piece length and hem allowance
+    pattern_height = shirt_length + 25 + 2.5 # add 6cm for hem and 2cm for collar piece
+    return pattern_height
+    
 def main():
     '''
     the main routine to analyze my scan data
@@ -73,6 +95,7 @@ def main():
 
     myscan_data = read_myscan_data()
     calculate_pattern_width(myscan_data)
+    calculate_pattern_height(myscan_data)
 
 # Execute main function
 if __name__ == "__main__":
